51a00cbe3a981047acbd809f056f0107b5ad86f7
Peach <k12112400@163.com>
2023-01-27 11:13:16 +0800

    second commit

--- src/main/java/com/yidaotaozi/git/GitTest.java
+++ src/main/java/com/yidaotaozi/git/GitTest.java
@@ -7,5 +7,6 @@ package com.yidaotaozi.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git2!");
     }
 }
