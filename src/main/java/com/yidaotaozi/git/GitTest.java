ce4bbabeaafc3f2d33e8ff4fe71b81aefc8c107b
Peach <k12112400@163.com>
2023-01-27 11:11:03 +0800

    first commit

---
+++ src/main/java/com/yidaotaozi/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yidaotaozi.git;
+
+/**
+ * @auther kjjstart
+ * @create 2023-01-27 11:05
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
